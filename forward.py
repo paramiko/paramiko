fb4e56989a72c5ea5d750cc61cf1a59dd874007e
Robey Pointer <robey@lag.net>
2005-07-09 17:30:02 +0000

    [project @ Arch-1:robey@lag.net--2005-master-shake%paramiko--dev--1--patch-27] remove now-incorrect comment

--- forward.py
+++ forward.py
@@ -24,8 +24,6 @@ Sample script showing how to do local port forwarding over paramiko.
 This script connects to the requested SSH server and sets up local port
 forwarding (the openssh -L option) from a local port through a tunneled
 connection to a destination reachable from the SSH server machine.
-
-It uses SocketServer and select, so may not work on Windows.
 """
 
 import sys
