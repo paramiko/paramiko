dd6fb2577d1c6f5cbfaf0264cc0452f465b0ca63
Jeff Forcier <jeff@bitprophet.org>
2012-09-23 14:01:57 -0700

    Bump to 0.8.x due to nontrivial changes (cherry picked from commit d7def60c4784fed39fc8714753304a150da131c7)
    Conflicts:
    	setup.py

--- paramiko/__init__.py
+++ paramiko/__init__.py
@@ -60,7 +60,7 @@ if sys.version_info < (2, 2):
 
 
 __author__ = "Jeff Forcier <jeff@bitprophet.org>"
-__version__ = "1.7.14"
+__version__ = "1.8.0"
 __license__ = "GNU Lesser General Public License (LGPL)"
 
 
