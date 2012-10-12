1b928df15ec4b95b0f96f26dc3740f1a0a91ca4a
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2012-10-12 09:51:18 +0200

    do not write ,,garbage'' to known_hosts file(s)

--- paramiko/client.py
+++ paramiko/client.py
@@ -193,7 +193,6 @@ class SSHClient (object):
             self.load_host_keys(self.known_hosts)
 
         f = open(filename, 'w')
-        f.write('# SSH host keys collected by paramiko\n')
         for hostname, keys in self._host_keys.iteritems():
             for keytype, key in keys.iteritems():
                 f.write('%s %s %s\n' % (hostname, keytype, key.get_base64()))
