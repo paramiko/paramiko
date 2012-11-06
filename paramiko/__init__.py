a3b44c7ed9a26231fcdf0e795305311da8814f92
Jeff Forcier <jeff@bitprophet.org>
2012-11-06 13:06:08 -0800

    Bump to 1.9.0 for release

--- paramiko/__init__.py
+++ paramiko/__init__.py
@@ -55,7 +55,7 @@ if sys.version_info < (2, 2):
 
 
 __author__ = "Jeff Forcier <jeff@bitprophet.org>"
-__version__ = "1.8.0"
+__version__ = "1.9.0"
 __license__ = "GNU Lesser General Public License (LGPL)"
 
 
