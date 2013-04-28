f861c2ff48f420b3be16f15bbf36c6ee3f779c38
Jeff Forcier <jeff@bitprophet.org>
2013-04-27 20:55:50 -0700

    Back out broken but non-required hostname hash change

--- paramiko/hostkeys.py
+++ paramiko/hostkeys.py
@@ -141,8 +141,6 @@ class HostKeys (UserDict.DictMixin):
             if (hostname in e.hostnames) and (e.key.get_name() == keytype):
                 e.key = key
                 return
-        if not hostname.startswith('|1|') and hash_hostname:
-            hostname = self.hash_host(hostname)
         self._entries.append(HostKeyEntry([hostname], key))
 
     def load(self, filename):
