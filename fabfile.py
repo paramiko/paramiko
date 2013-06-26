993ecb31d2a0606cb1e95e85dd29ea2809866663
Jeff Forcier <jeff@bitprophet.org>
2013-06-26 09:08:42 -0700

    Port Makefile contents into fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
-from fabric.api import task, sudo, env
+from fabric.api import task, sudo, env, local
 from fabric.contrib.project import rsync_project
+from fabric.contrib.console import confirm
 
 
 @task
@@ -11,3 +12,26 @@ def upload_docs():
     sudo("rm -rf %s/*" % target)
     rsync_project(local_dir='docs/', remote_dir=staging, delete=True)
     sudo("cp -R %s/* %s/" % (staging, target))
+
+@task
+def build_docs():
+    local("epydoc --no-private -o docs/ paramiko")
+
+@task
+def clean():
+    local("rm -rf build dist docs")
+    local("rm -f MANIFEST *.log demos/*.log")
+    local("rm -f paramiko/*.pyc")
+    local("rm -f test.log")
+    local("rm -rf paramiko.egg-info")
+
+@task
+def test():
+    local("python ./test.py")
+
+@task
+def release():
+    confirm("Only hit Enter if you remembered to update the version!")
+    build_docs()
+    local("python setup.py sdist register upload")
+    upload_docs()
