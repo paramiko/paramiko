21984ec8c79c1d0bcdf5376e4f2c40c3974b0825
Michael Schrijver <michael@hating.biz>
2013-07-25 13:50:23 +0200

    Fix hang when sending packets on closed connection

--- paramiko/packet.py
+++ paramiko/packet.py
@@ -265,7 +265,7 @@ class Packetizer (object):
                 n = 0
                 if self.__closed:
                     n = -1
-            if n < 0:
+            if n <= 0:
                 raise EOFError()
             if n == len(out):
                 break
