bc683ac3653e2d66fe18c020374ec5b7c723ed53
Scott Maxwell <scott@codecobblers.com>
2013-10-31 15:35:35 -0700

    Fix demo_server

--- demos/demo_server.py
+++ demos/demo_server.py
@@ -27,6 +27,7 @@ import threading
 import traceback
 
 import paramiko
+from paramiko.py3compat import b, u
 
 
 # setup logging
@@ -35,16 +36,16 @@ paramiko.util.log_to_file('demo_server.log')
 host_key = paramiko.RSAKey(filename='test_rsa.key')
 #host_key = paramiko.DSSKey(filename='test_dss.key')
 
-print 'Read key: ' + hexlify(host_key.get_fingerprint())
+print('Read key: ' + u(hexlify(host_key.get_fingerprint())))
 
 
 class Server (paramiko.ServerInterface):
     # 'data' is the output of base64.encodestring(str(key))
     # (using the "user_rsa_key" files)
-    data = 'AAAAB3NzaC1yc2EAAAABIwAAAIEAyO4it3fHlmGZWJaGrfeHOVY7RWO3P9M7hp' + \
-           'fAu7jJ2d7eothvfeuoRFtJwhUmZDluRdFyhFY/hFAh76PJKGAusIqIQKlkJxMC' + \
-           'KDqIexkgHAfID/6mqvmnSJf0b5W8v5h2pI/stOSwTQ+pxVhwJ9ctYDhRSlF0iT' + \
-           'UWT10hcuO4Ks8='
+    data = b('AAAAB3NzaC1yc2EAAAABIwAAAIEAyO4it3fHlmGZWJaGrfeHOVY7RWO3P9M7hp' + \
+             'fAu7jJ2d7eothvfeuoRFtJwhUmZDluRdFyhFY/hFAh76PJKGAusIqIQKlkJxMC' + \
+             'KDqIexkgHAfID/6mqvmnSJf0b5W8v5h2pI/stOSwTQ+pxVhwJ9ctYDhRSlF0iT' + \
+             'UWT10hcuO4Ks8=')
     good_pub_key = paramiko.RSAKey(data=base64.decodestring(data))
 
     def __init__(self):
@@ -61,7 +62,7 @@ class Server (paramiko.ServerInterface):
         return paramiko.AUTH_FAILED
 
     def check_auth_publickey(self, username, key):
-        print 'Auth attempt with key: ' + hexlify(key.get_fingerprint())
+        print('Auth attempt with key: ' + u(hexlify(key.get_fingerprint())))
         if (username == 'robey') and (key == self.good_pub_key):
             return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
@@ -83,47 +84,49 @@ try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('', 2200))
-except Exception, e:
-    print '*** Bind failed: ' + str(e)
+except Exception:
+    e = sys.exc_info()[1]
+    print('*** Bind failed: ' + str(e))
     traceback.print_exc()
     sys.exit(1)
 
 try:
     sock.listen(100)
-    print 'Listening for connection ...'
+    print('Listening for connection ...')
     client, addr = sock.accept()
-except Exception, e:
-    print '*** Listen/accept failed: ' + str(e)
+except Exception:
+    e = sys.exc_info()[1]
+    print('*** Listen/accept failed: ' + str(e))
     traceback.print_exc()
     sys.exit(1)
 
-print 'Got a connection!'
+print('Got a connection!')
 
 try:
     t = paramiko.Transport(client)
     try:
         t.load_server_moduli()
     except:
-        print '(Failed to load moduli -- gex will be unsupported.)'
+        print('(Failed to load moduli -- gex will be unsupported.)')
         raise
     t.add_server_key(host_key)
     server = Server()
     try:
         t.start_server(server=server)
-    except paramiko.SSHException, x:
-        print '*** SSH negotiation failed.'
+    except paramiko.SSHException:
+        print('*** SSH negotiation failed.')
         sys.exit(1)
 
     # wait for auth
     chan = t.accept(20)
     if chan is None:
-        print '*** No channel.'
+        print('*** No channel.')
         sys.exit(1)
-    print 'Authenticated!'
+    print('Authenticated!')
 
     server.event.wait(10)
     if not server.event.isSet():
-        print '*** Client never asked for a shell.'
+        print('*** Client never asked for a shell.')
         sys.exit(1)
 
     chan.send('\r\n\r\nWelcome to my dorky little BBS!\r\n\r\n')
@@ -135,8 +138,9 @@ try:
     chan.send('\r\nI don\'t like you, ' + username + '.\r\n')
     chan.close()
 
-except Exception, e:
-    print '*** Caught exception: ' + str(e.__class__) + ': ' + str(e)
+except Exception:
+    e = sys.exc_info()[1]
+    print('*** Caught exception: ' + str(e.__class__) + ': ' + str(e))
     traceback.print_exc()
     try:
         t.close()
