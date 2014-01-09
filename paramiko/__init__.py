698adf10fb68330e1439b60afe9d305014395a68
Jeff Forcier <jeff@bitprophet.org>
2014-01-08 16:40:11 -0800

    Cut 1.10.5

--- paramiko/__init__.py
+++ paramiko/__init__.py
@@ -55,7 +55,7 @@ if sys.version_info < (2, 5):
 
 
 __author__ = "Jeff Forcier <jeff@bitprophet.org>"
-__version__ = "1.10.4"
+__version__ = "1.10.5"
 __version_info__ = tuple([ int(d) for d in __version__.split(".") ])
 __license__ = "GNU Lesser General Public License (LGPL)"
 
