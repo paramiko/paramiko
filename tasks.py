c224fdecf174077f3b7a15f056e65b10282fed38
Jeff Forcier <jeff@bitprophet.org>
2014-01-13 16:16:30 -0800

    Use new behavior from newer Invoke

--- tasks.py
+++ tasks.py
@@ -2,17 +2,13 @@ from invoke import Collection
 from invocations import docs, testing
 
 
-# TODO: let from_module specify new name
-api = Collection.from_module(docs)
-# TODO: maybe allow rolling configuration into it too heh
-api.configure({
+# Usage doc/API site
+api = Collection.from_module(docs, name='docs', config={
     'sphinx.source': 'sites/docs',
     'sphinx.target': 'sites/docs/_build',
 })
-api.name = 'docs'
-main = Collection.from_module(docs)
-main.name = 'main'
-main.configure({
+# Main/about/changelog site
+main = Collection.from_module(docs, name='main', config={
     'sphinx.source': 'sites/main',
     'sphinx.target': 'sites/main/_build',
 })
