23bb7e593647d0021aa68513fb881c79d807ca0b
Jeff Forcier <jeff@bitprophet.org>
2014-02-24 09:59:40 -0800

    Nuke useless module docstring

--- paramiko/proxy.py
+++ paramiko/proxy.py
@@ -16,9 +16,6 @@
 # along with Paramiko; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
-"""
-`.ProxyCommand`.
-"""
 
 import os
 from shlex import split as shlsplit
