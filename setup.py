951faed80b017e553a27c4cb98f210df44341f8f
Jeff Forcier <jeff@bitprophet.org>
2014-05-07 16:13:33 -0700

    Cut 1.14

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if sys.platform == 'darwin':
 
 setup(
     name = "paramiko",
-    version = "1.13.1",
+    version = "1.14.0",
     description = "SSH2 protocol library",
     long_description = longdesc,
     author = "Jeff Forcier",
