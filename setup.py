075f5c0338cf48e2b0280dd97b35aa6e40d94b3d
Olle Lundberg <geek@nerd.sh>
2014-08-14 12:03:32 +0200

    Support py3.4.

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     **kw
 )
