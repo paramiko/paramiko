233f0a422e3384584e20eff7330a536230d3cd31
Jeff Forcier <jeff@bitprophet.org>
2014-09-18 17:54:20 -0700

    Fix more missed epydoc syntax

--- paramiko/file.py
+++ paramiko/file.py
@@ -104,14 +104,13 @@ class BufferedFile (object):
     else:
         def __next__(self):
             """
-            Returns the next line from the input, or raises L{StopIteration} when
+            Returns the next line from the input, or raises `.StopIteration` when
             EOF is hit.  Unlike python file objects, it's okay to mix calls to
-            C{next} and L{readline}.
+            `.next` and `.readline`.
 
-            @raise StopIteration: when the end of the file is reached.
+            :raises StopIteration: when the end of the file is reached.
 
-            @return: a line read from the file.
-            @rtype: str
+            :returns: a line (`str`) read from the file.
             """
             line = self.readline()
             if not line:
--- paramiko/server.py
+++ paramiko/server.py
@@ -473,21 +473,18 @@ class ServerInterface (object):
     def check_channel_env_request(self, channel, name, value):
         """
         Check whether a given environment variable can be specified for the
-        given channel.  This method should return C{True} if the server
+        given channel.  This method should return ``True`` if the server
         is willing to set the specified environment variable.  Note that
         some environment variables (e.g., PATH) can be exceedingly
         dangerous, so blindly allowing the client to set the environment
         is almost certainly not a good idea.
 
-        The default implementation always returns C{False}.
+        The default implementation always returns ``False``.
 
-        @param channel: the L{Channel} the env request arrived on
-        @type channel: L{Channel}
-        @param name: foo bar baz
-        @type name: str
-        @param value: flklj
-        @type value: str
-        @rtype: bool
+        :param channel: the `.Channel` the env request arrived on
+        :param str name: name
+        :param str value: Channel value
+        :returns: A boolean
         """
         return False
 
