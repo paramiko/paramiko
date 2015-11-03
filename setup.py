583d6eaedfe2b004777b100ab8a8185bb9bec073
Jeff Forcier <jeff@bitprophet.org>
2015-11-02 17:32:00 -0800

    We ought to work on python 3.4/3.5...

--- setup.py
+++ setup.py
@@ -86,6 +86,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     **kw
 )
