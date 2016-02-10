587804d577b317fb7a58f8be6c7853a3911a3e41
Winston Ewert <winstonewert@gmail.com>
2016-02-10 11:25:27 -0800

    Fixes NoValidConnectionsError on python3

---
+++ tests/test_ssh_exception.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2008  Robey Pointer <robeypointer@gmail.com>
+#
+# This file is part of paramiko.
+#
+# Paramiko is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+"""
+Some unit tests for exceptions
+"""
+
+import unittest
+
+from paramiko.ssh_exception import NoValidConnectionsError
+
+class SSHExceptionTest (unittest.TestCase):
+
+    def test_1_create_no_valid_connections_exception(self):
+        """
+        Veryify that creating this exception does not fail
+        """
+        NoValidConnectionsError({b'127.0.0.1': None})
