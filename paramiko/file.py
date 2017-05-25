e66899a87ed2e510b66116284fc95763f5a2f4ec
Martin Packman <gzlist@googlemail.com>
2017-05-25 20:37:14 +0100

    Allow any buffer type to written to BufferedFile
    Fixes #967
    
    Also adds test coverage for writing various types to
    BufferedFile which required some small changes to the test
    LoopbackFile subclass.
    
    Change against the 1.17 branch.

--- paramiko/file.py
+++ paramiko/file.py
@@ -18,7 +18,7 @@
 from paramiko.common import (
     linefeed_byte_value, crlf, cr_byte, linefeed_byte, cr_byte_value,
 )
-from paramiko.py3compat import BytesIO, PY2, u, b, bytes_types
+from paramiko.py3compat import BytesIO, PY2, u, bytes_types, text_type
 
 from paramiko.util import ClosingContextManager
 
@@ -391,7 +391,10 @@ class BufferedFile (ClosingContextManager):
 
         :param data: ``str``/``bytes`` data to write
         """
-        data = b(data)
+        if isinstance(data, text_type):
+            # GZ 2017-05-25: Accepting text on a binary stream unconditionally
+            # cooercing to utf-8 seems questionable, but compatibility reasons?
+            data = data.encode('utf-8')
         if self._closed:
             raise IOError('File is closed')
         if not (self._flags & self.FLAG_WRITE):
