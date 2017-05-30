5e85d548e15e8f1b0a7d74a922d56a6026e0b2d4
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2017-05-30 12:26:26 -0400

    Final paramiko flake8 fix.

--- paramiko/__init__.py
+++ paramiko/__init__.py
@@ -16,6 +16,7 @@
 # along with Paramiko; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
+# flake8: noqa
 import sys
 from paramiko._version import __version__, __version_info__
 
