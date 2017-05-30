7fb89cf56da37d906595bb6dd9c88250fcfc6b25
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2017-05-30 12:27:54 -0400

    Final paramiko flake8 fix.

--- paramiko/transport.py
+++ paramiko/transport.py
@@ -16,6 +16,7 @@
 # along with Paramiko; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
+# flake8: noqa
 """
 Core protocol implementation
 """
