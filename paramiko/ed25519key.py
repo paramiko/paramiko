a2de0ad4e5aa9ce2a98ed0009990d525f744073b
Jeff Forcier <jeff@bitprophet.org>
2017-09-05 19:02:41 -0700

    Document Ed25519 keys =/
    I didn't badger people about docs so there were none

--- paramiko/ed25519key.py
+++ paramiko/ed25519key.py
@@ -45,6 +45,16 @@ def unpad(data):
 
 
 class Ed25519Key(PKey):
+    """
+    Representation of an `Ed25519 <https://ed25519.cr.yp.to/>`_ key.
+
+    .. note::
+        Ed25519 key support was added to OpenSSH in version 6.5.
+
+    .. versionadded:: 2.2
+    .. versionchanged:: 2.3
+        Added a ``file_obj`` parameter to match other key classes.
+    """
     def __init__(self, msg=None, data=None, filename=None, password=None,
                  file_obj=None):
         verifying_key = signing_key = None
