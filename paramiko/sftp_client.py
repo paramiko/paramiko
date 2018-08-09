17c39fbf1d8d131761294a53f4bd87f8d84dc075
Gordon P. Hemsley <me@gphemsley.org>
2018-08-08 22:17:05 -0400

    Fixes #1255: Add os.path-like stat-based methods to SFTPClient
    Add methods to `SFTPClient` that resemble equivalent methods from
    `os.path`:
     * `exists`
     * `lexists`
     * `isfile`
     * `islink`
     * `isdir`
    
    All of these methods are based on the results of calling `stat` on a
    path, except `lexists`, which is based on `lstat`.
    
    These methods reduce the need for a user of the paramiko library
    to import the Python `stat` module and inspect the return value of
    an `SFTPClient.stat` call.

--- paramiko/sftp_client.py
+++ paramiko/sftp_client.py
@@ -444,6 +444,159 @@ class SFTPClient(BaseSFTP, ClosingContextManager):
             CMD_EXTENDED, "posix-rename@openssh.com", oldpath, newpath
         )
 
+    def exists(self, path):
+        """
+        Check a path to determine whether it exists, based on `stat`.
+        This follows symlinks, so a broken symlink will return ``False``.
+
+        :param str path: path to check
+
+        :rtype: bool
+        :return: ``True`` if the path exists and is not a broken symlink;
+            ``False`` otherwise
+
+        .. versionadded:: 2.5
+        """
+        path = self._adjust_cwd(path)
+        self._log(DEBUG, "exists({!r})".format(path))
+
+        try:
+            self.stat(path)
+        except (OSError, IOError) as e:
+            self._log(
+                DEBUG,
+                "{}: {} ({!r})".format(
+                    type(e).__name__,
+                    e.strerror,
+                    e.filename if e.filename is not None else path,
+                ),
+            )
+            return False
+
+        return True
+
+    def lexists(self, path):
+        """
+        Check a path to determine whether it exists, based on `lstat`.
+        This does not follow symlinks, so a broken symlink will return
+        ``True``.
+
+        :param str path: path to check
+
+        :rtype: bool
+        :return: ``True`` if the path exists;
+            ``False`` otherwise
+
+        .. versionadded:: 2.5
+        """
+        path = self._adjust_cwd(path)
+        self._log(DEBUG, "exists({!r})".format(path))
+
+        try:
+            self.lstat(path)
+        except (OSError, IOError) as e:
+            self._log(
+                DEBUG,
+                "{}: {} ({!r})".format(
+                    type(e).__name__,
+                    e.strerror,
+                    e.filename if e.filename is not None else path,
+                ),
+            )
+            return False
+
+        return True
+
+    def isfile(self, path):
+        """
+        Check a path to determine whether it exists and is a file.
+
+        :param str path: path to check
+
+        :rtype: bool
+        :return: ``True`` if the path exists and is a file;
+            ``False`` otherwise
+
+        .. versionadded:: 2.5
+        """
+        path = self._adjust_cwd(path)
+        self._log(DEBUG, "isfile({!r})".format(path))
+
+        try:
+            path_stat = self.stat(path)
+        except (OSError, IOError) as e:
+            self._log(
+                DEBUG,
+                "{}: {} ({!r})".format(
+                    type(e).__name__,
+                    e.strerror,
+                    e.filename if e.filename is not None else path,
+                ),
+            )
+            return False
+
+        return stat.S_ISREG(path_stat.st_mode)
+
+    def islink(self, path):
+        """
+        Check a path to determine whether it exists and is a symlink.
+
+        :param str path: path to check
+
+        :rtype: bool
+        :return: ``True`` if the path exists and is a symlink;
+            ``False`` otherwise
+
+        .. versionadded:: 2.5
+        """
+        path = self._adjust_cwd(path)
+        self._log(DEBUG, "isfile({!r})".format(path))
+
+        try:
+            path_lstat = self.lstat(path)
+        except (OSError, IOError) as e:
+            self._log(
+                DEBUG,
+                "{}: {} ({!r})".format(
+                    type(e).__name__,
+                    e.strerror,
+                    e.filename if e.filename is not None else path,
+                ),
+            )
+            return False
+
+        return stat.S_ISLNK(path_lstat.st_mode)
+
+    def isdir(self, path):
+        """
+        Check a path to determine whether it exists and is a directory.
+
+        :param str path: path to check
+
+        :rtype: bool
+        :return: ``True`` if the path exists and is a directory;
+            ``False`` otherwise
+
+        .. versionadded:: 2.5
+        """
+        path = self._adjust_cwd(path)
+        self._log(DEBUG, "isfile({!r})".format(path))
+
+        try:
+            path_stat = self.stat(path)
+        except (OSError, IOError) as e:
+            self._log(
+                DEBUG,
+                "{}: {} ({!r})".format(
+                    type(e).__name__,
+                    e.strerror,
+                    e.filename if e.filename is not None else path,
+                ),
+            )
+            return False
+
+        return stat.S_ISDIR(path_stat.st_mode)
+
     def mkdir(self, path, mode=o777):
         """
         Create a folder (directory) named ``path`` with numeric mode ``mode``.
