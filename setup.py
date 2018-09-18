8cc565e3302467e076068b907b06ee4738fb3eb3
Jon Dufresne <jon.dufresne@gmail.com>
2018-09-18 06:50:34 -0700

    setup.py: add python_requires
    Helps pip decide what version of the library to install.
    
    PEP-345
    https://packaging.python.org/tutorials/distributing-packages/#python-requires

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=[
         'bcrypt>=3.1.3',
         'cryptography>=1.5',
