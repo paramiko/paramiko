87a3c1aa2d3c4efb52462dd4cd1ac8e19cf20c3b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-10-26 11:20:41 -0500

    Help users ignore unimportant errors
    Offer an SSH key policy for users who want to ignore unregistered key errors

--- paramiko/client.py
+++ paramiko/client.py
@@ -828,3 +828,12 @@ class WarningPolicy(MissingHostKeyPolicy):
                 key.get_name(), hostname, hexlify(key.get_fingerprint())
             )
         )
+
+
+class IgnorePolicy(paramiko.MissingHostKeyPolicy):
+    """
+    Policy for ignoring missing host key.
+    """
+
+    def missing_host_key(self, client, hostname, key):
+        pass
