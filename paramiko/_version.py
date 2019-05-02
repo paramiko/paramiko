6961a1744d3625475b3a545a11d69dbd570b4df9
Pierce Lopez <pierce.lopez@gmail.com>
2019-05-02 14:36:40 -0400

    bump version to 2.5.1 for development
    new scheme where if last version component is odd it's pre-release

--- paramiko/_version.py
+++ paramiko/_version.py
@@ -1,2 +1,3 @@
-__version_info__ = (2, 5, 0)
+# last version component is odd for pre-release development, even for stable release
+__version_info__ = (2, 5, 1)
 __version__ = '.'.join(map(str, __version_info__))
