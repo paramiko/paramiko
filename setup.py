f9ed04eddf6e6b919dfe0872e753c5600cd97f99
Jeff Forcier <jeff@bitprophet.org>
2019-05-31 18:28:33 -0400

    Add explicit (tested) support for Python 3.8
    Cannot be added to Paramiko <2.4 due to Travis
    inability to test Python 2.6->3.7+

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=[
         "bcrypt>=3.1.3",
