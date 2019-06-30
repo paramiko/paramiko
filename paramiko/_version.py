6d2c64a05c87bc6c26505586bee8efe7d9f267a2
Pierce Lopez <pierce.lopez@gmail.com>
2019-06-29 20:56:50 -0400

    version 2.7.2

--- paramiko/_version.py
+++ paramiko/_version.py
@@ -1,3 +1,3 @@
 # last version component is odd for pre-release development, even for stable release
-__version_info__ = (2, 7, 1)
+__version_info__ = (2, 7, 2)
 __version__ = '.'.join(map(str, __version_info__))
