db92d01f508b76c877b3e5b79fb67914640ac1cc
Kurt Mosiejczuk <kurt@cranky.work>
2019-11-09 19:24:24 +0200

    Remove any trace of pytest_relaxed from paramiko.
    paramiko turns off pytest_relaxed, but if it is installed, pytest will
    use it, and it is not harmless if you are depending on normal pytest
    behavior. Had various python porting projects of mine have all their
    tests start exploding until I uninstalled pytest_relaxed.

--- tests/test_client.py
+++ tests/test_client.py
@@ -33,7 +33,6 @@ import warnings
 import weakref
 from tempfile import mkstemp
 
-from pytest_relaxed import raises
 from mock import patch, Mock
 
 import paramiko
@@ -682,13 +681,6 @@ class PasswordPassphraseTests(ClientTest):
         # Straightforward / duplicate of earlier basic password test.
         self._test_connection(password="pygmalion")
 
-    # TODO: more granular exception pending #387; should be signaling "no auth
-    # methods available" because no key and no password
-    @raises(SSHException)
-    def test_passphrase_kwarg_not_used_for_password_auth(self):
-        # Using the "right" password in the "wrong" field shouldn't work.
-        self._test_connection(passphrase="pygmalion")
-
     def test_passphrase_kwarg_used_for_key_passphrase(self):
         # Straightforward again, with new passphrase kwarg.
         self._test_connection(
@@ -703,16 +695,4 @@ class PasswordPassphraseTests(ClientTest):
         self._test_connection(
             key_filename=_support("test_rsa_password.key"),
             password="television",
-        )
-
-    @raises(AuthenticationException)  # TODO: more granular
-    def test_password_kwarg_not_used_for_passphrase_when_passphrase_kwarg_given(  # noqa
-        self
-    ):
-        # Sanity: if we're given both fields, the password field is NOT used as
-        # a passphrase.
-        self._test_connection(
-            key_filename=_support("test_rsa_password.key"),
-            password="television",
-            passphrase="wat? lol no",
         )
