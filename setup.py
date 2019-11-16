8e56e81485694578c44213f53ef785f8cdf98abb
Pierce Lopez <pierce.lopez@gmail.com>
2019-11-16 15:03:56 -0500

    setup: explicit mention of Python-3.8 support

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=[
