862f761f663d04328c19f616040e54facc648ac3
Pierce Lopez <pierce.lopez@gmail.com>
2020-01-20 04:53:15 -0500

    bump to version 2.7.9 for development leading to 2.8.0

--- paramiko/_version.py
+++ paramiko/_version.py
@@ -1,3 +1,3 @@
 # last version component is odd for pre-release development, even for stable release
-__version_info__ = (2, 7, 5)
+__version_info__ = (2, 7, 9)
 __version__ = '.'.join(map(str, __version_info__))
