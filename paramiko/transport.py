02d7b19a0239b4b323dba8895ec60df07d75aa2f
Manfred Kaiser <37737811+manfred-kaiser@users.noreply.github.com>
2021-03-07 08:47:16 +0100

    Update transport.py

--- paramiko/transport.py
+++ paramiko/transport.py
@@ -46,6 +46,7 @@ from paramiko.common import (
     DEBUG,
     MSG_KEXINIT,
     MSG_IGNORE,
+    MSG_KEXSKIP,
     MSG_DISCONNECT,
     MSG_DEBUG,
     ERROR,
@@ -2065,7 +2066,7 @@ class Transport(threading.Thread, ClosingContextManager):
                         continue
                     if len(self._expected_packet) > 0:
                         if ptype not in self._expected_packet:
-                            if ptype == 30:
+                            if ptype == MSG_KEXSKIP:
                                 # according to rfc 4253, the next packet should be ignored,
                                 # when first_kex_packet_follows is True
                                 # this is a workarround at the moment, but connection works
