534700c80185e1365f2fcae0f018df08efb8c05f
Jeff Forcier <jeff@bitprophet.org>
2021-10-01 18:34:21 -0400

    Add Py3.9 to classifiers

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     # TODO 3.0: remove bcrypt, pynacl and update installation docs noting that
     # use of the extras_require(s) is now required for those
