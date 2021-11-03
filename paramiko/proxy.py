f8d36f129f512e7cb6143ede6a7578a6cf1af386
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2021-11-03 17:19:59 +0100

    fix proxy for MS Windows

--- paramiko/proxy.py
+++ paramiko/proxy.py
@@ -17,7 +17,7 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 
-import os
+import os, sys
 import shlex
 import signal
 from select import select
@@ -88,12 +88,17 @@ class ProxyCommand(ClosingContextManager):
     def recv(self, size):
         """
         Read from the standard output of the forked program.
+        On Windows select() only works on sockets, so the loop will fail on this 
+        platform. Therefore, use a simple blocking call in this case.
 
         :param int size: how many chars should be read
 
         :return: the string of bytes read, which may be shorter than requested
         """
         try:
+            if sys.platform == 'win32':
+                return os.read(self.process.stdout.fileno(), size)
+
             buffer = b""
             start = time.time()
             while len(buffer) < size:
