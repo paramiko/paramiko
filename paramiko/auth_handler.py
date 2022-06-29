40d61f06e32c885fb9affe44a7d85f6f35229076
Alan Somers <asomers@gmail.com>
2022-06-29 15:18:44 -0600

    Host based authentication
    Updated version of https://github.com/paramiko/paramiko/pull/1456 with
    Python 3 support, ecdsa and ed25519 support, and a few other fixes.
    
    Fixes #316

--- paramiko/auth_handler.py
+++ paramiko/auth_handler.py
@@ -20,6 +20,8 @@
 `.AuthHandler`
 """
 
+import getpass
+import socket
 import weakref
 import time
 import re
@@ -62,7 +64,7 @@ from paramiko.common import (
     cMSG_USERAUTH_BANNER,
 )
 from paramiko.message import Message
-from paramiko.py3compat import b, u
+from paramiko.py3compat import byte_chr, b, u
 from paramiko.ssh_exception import (
     SSHException,
     AuthenticationException,
@@ -70,6 +72,7 @@ from paramiko.ssh_exception import (
     PartialAuthentication,
 )
 from paramiko.server import InteractiveQuery
+from paramiko.keysign import Keysign
 from paramiko.ssh_gss import GSSAuth, GSS_EXCEPTIONS
 
 
@@ -84,6 +87,7 @@ class AuthHandler(object):
         self.authenticated = False
         self.auth_event = None
         self.auth_method = ""
+        self.hostkey = None
         self.banner = None
         self.password = None
         self.private_key = None
@@ -177,6 +181,17 @@ class AuthHandler(object):
         finally:
             self.transport.lock.release()
 
+    def auth_hostbased(self, username, hostkey, event):
+        self.transport.lock.acquire()
+        try:
+            self.auth_event = event
+            self.auth_method = "hostbased"
+            self.username = username
+            self.hostkey = hostkey
+            self._request_auth()
+        finally:
+            self.transport.lock.release()
+
     def abort(self):
         if self.auth_event is not None:
             self.auth_event.set()
@@ -231,6 +246,24 @@ class AuthHandler(object):
         m.add_string(algorithm)
         m.add_string(bits)
         return m.asbytes()
+
+    def _get_hostbased_session_blob(self, hostkey, service, username):
+        m = Message()
+        m.add_string(self.transport.session_id)
+        m.add_byte(byte_chr(MSG_USERAUTH_REQUEST))
+        m.add_string(username)
+        m.add_string(service)
+        m.add_string("hostbased")
+        m.add_string(hostkey.get_name())
+        m.add_string(hostkey.asbytes())
+        m.add_string(self._get_local_name() + ".")
+        m.add_string(getpass.getuser())
+        return m
+
+    def _get_local_name(self):
+        addr = self.transport.sock.getsockname()
+        names = socket.getnameinfo(addr, socket.NI_NAMEREQD)
+        return names[0]
 
     def wait_for_response(self, event):
         max_ts = None
@@ -488,6 +521,17 @@ Error Message: {}
                 kexgss.set_username(self.username)
                 mic_token = kexgss.ssh_get_mic(self.transport.session_id)
                 m.add_string(mic_token)
+            elif self.auth_method == "hostbased":
+                m.add_string(self.hostkey.get_name())
+                m.add_string(self.hostkey.asbytes())
+                m.add_string(self._get_local_name() + ".")
+                m.add_string(getpass.getuser())
+                # this blob must be the same as the message (minus the sig)
+                blob = self._get_hostbased_session_blob(
+                    self.hostkey, "ssh-connection", self.username
+                )
+                sig = Keysign().sign(self.transport.sock, blob)
+                m.add_string(sig)
             elif self.auth_method == "none":
                 pass
             else:
