95d5159e62a4582009c9ec8f0249399c1d1eebba
Ján Jančár <J08nY@users.noreply.github.com>
2024-10-09 09:23:32 +0000

    Add check for public_blob attr.
    Fixes #2462.

--- paramiko/auth_handler.py
+++ paramiko/auth_handler.py
@@ -215,7 +215,7 @@ class AuthHandler:
         Intended for input to or verification of, key signatures.
         """
         # Use certificate contents, if available, plain pubkey otherwise
-        if key.public_blob:
+        if hasattr(key, "public_blob"):
             return key.public_blob.key_type, key.public_blob.key_blob
         else:
             return key.get_name(), key
