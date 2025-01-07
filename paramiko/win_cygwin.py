398cfc995bd17557c0f9c9b0ecdd576dfe3c1e87
HuangWei <sir.huangwei@gmail.com>
2025-01-07 08:48:16 +0800

    Update win_cygwin.py for style check

--- paramiko/win_cygwin.py
+++ paramiko/win_cygwin.py
@@ -23,20 +23,20 @@ import struct
 
 
 def can_talk_to_agent():
-    pipe_name = os.environ.get('SSH_AUTH_SOCK')
+    pipe_name = os.environ.get("SSH_AUTH_SOCK")
     dir_, name = os.path.split(pipe_name)
     name = name.lower()
     return any(name == n.lower() for n in os.listdir(dir_))
 
 def conv_guid(s):
-    parts = s.split('-')
+    parts = s.split("-")
     int_parts = [int(part, 16) for part in parts]
-    return b''.join([struct.pack('<I', part) for part in int_parts])
+    return b"".join([struct.pack("<I"), part) for part in int_parts])
 
 
 # https://stackoverflow.com/questions/23086038/what-mechanism-is-used-by-msys-cygwin-to-emulate-unix-domain-sockets
 def read_socket_info(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         data = f.read().strip()
     # cygwin implementation
     # !<socket >59108 s 282F93E1-9E2D051A-46B57EFC-64A1852F
@@ -48,7 +48,7 @@ def read_socket_info(file_path):
     else:
         tag, port, guid = parts
     port = int(port[1:])
-    guid = guid[:-1] # cut \x00
+    guid = guid[:-1]  # cut \x00
 
     guid_bytes = conv_guid(guid)
     return guid_bytes, port
@@ -57,11 +57,11 @@ def read_socket_info(file_path):
 class OpenCygwinSSHAgentConnection:
     def __init__(self):
         try:
-            pipe_name = os.environ.get('SSH_AUTH_SOCK')
+            pipe_name = os.environ.get("SSH_AUTH_SOCK")
             guid, port = read_socket_info(pipe_name)
 
             self._conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self._conn.connect(('localhost', port))
+            self._conn.connect(("localhost", port))
 
             # sends the 4 random numbers GUID to the server
             self._conn.sendall(guid)
@@ -75,10 +75,10 @@ class OpenCygwinSSHAgentConnection:
             pid = os.getpid()
             uid = 0
             gid = 0
-            self._conn.sendall(struct.pack('iii', pid, uid, gid))
+            self._conn.sendall(struct.pack("iii", pid, uid, gid))
 
             data = self._conn.recv(12)
-            s_pid, s_uid, s_gid = struct.unpack('iii', data)
+            s_pid, s_uid, s_gid = struct.unpack("iii", data)
 
         except:
             self._conn.close()
